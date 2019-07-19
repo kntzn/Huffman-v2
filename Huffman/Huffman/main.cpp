aa7b97f2a9455b9edbbb8781197842f431719cf6
kntzn <kntznart@gmail.com>
2019-07-19 20:56:51 +0300

    [Imp]: freq_table

--- Huffman/Huffman/main.cpp
+++ Huffman/Huffman/main.cpp
@@ -3,19 +3,31 @@
 
 #define N_CHARS 256
 
+struct CharFreq
+    {
+    unsigned long freq;
+    char ch;
+    };
+
 int main ()
     { 
     char string [] = "beep boop beer!";
     unsigned long len = 16;
 
-    int freq_table [N_CHARS] = { };
+    // Sets up freq_table and sets chars
+    CharFreq freq_table [N_CHARS] = { };
+    for (int i = 0; i < N_CHARS; i++)
+        freq_table [i].ch = i;
+
+    // Counts frequency
     for (int i = 0; i < len; i++)
-        freq_table [string [i]]++;
+        freq_table [string [i]].freq++;
+
 
     for (int i = 0; i < N_CHARS; i++)
         { 
-        if (freq_table [i])
-            std::cout << char (i) << " (" << i << ") - " << freq_table [i] << std::endl;
+        if (freq_table [i].freq)
+            std::cout << char (i) << " (" << i << ") - " << freq_table [i].freq << std::endl;
         }
 
     system ("pause");
